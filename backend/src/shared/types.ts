616aa8ab8606e8cc642ccae8ea92b6a8e2d8e744
Rorisang Putu <rorisangputu@gmail.com>
2025-04-08 20:24:23 +0200

    creating vendor type

---
+++ backend/src/shared/types.ts
@@ -0,0 +1,14 @@
+export type VendorType = {
+    name: string;
+    fuelTypes: [string];
+    address: string;
+    phone: string;
+    email: string;
+    password: string;
+    pincode: string;
+    salt: string;
+    serviceAvailable: boolean;
+    rating: number;
+    coverImage: string;
+    imageUrls: [string];
+}
